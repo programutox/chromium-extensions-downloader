76b8a69d2128edc9510a4eec0c85f3a6b29eed85
programutox <programutox@disroot.org>
2024-03-15 18:17:03 +0100

    The program now accepts the extension link instead of the id

--- src/chromium_extensions_downloader.nim
+++ src/chromium_extensions_downloader.nim
@@ -1,11 +1,16 @@
-from os import commandLineParams
-from strformat import fmt
+from std/os import commandLineParams
+from std/strformat import fmt
+import std/strutils
+
+const
+  attributeLength = 6
+  extensionIdLength = 32
 
 proc main =
   let params = commandLineParams()
   if params.len == 0 or params[0] == "help":
     echo "CED (Chromium Extensions Downloader) by programutox"
-    echo "Command expected : ced chromium_version extension_id"
+    echo "Command expected : ced chromium_version extension_link"
     echo "Returns : a link you can paste to download an extension without going to the web store"
     return
 
@@ -13,7 +18,17 @@ proc main =
     echo "Incorrect number of arguments (must be 2)"
     return
 
-  echo fmt"https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion={params[0]}&x=id%3D{params[1]}%26installsource%3Dondemand%26uc"
+  let
+    chromiumVersion = params[0]
+    extensionLink = params[1]
+    extensionId =
+      # If the link ends with attribute "?hl=en" for example
+      if "?hl=" in extensionLink:
+        extensionLink[extensionLink.len-extensionIdLength-attributeLength..extensionLink.len-attributeLength-1]
+      else:
+        extensionLink[extensionLink.len-extensionIdLength..extensionLink.len-1]
+
+  echo fmt"https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion={chromiumVersion}&x=id%3D{extensionId}%26installsource%3Dondemand%26uc"
 
 when isMainModule:
   main()
